11f9b4596d084527ca074f36aa42f28ecf326964
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2024-12-02 10:17:14 -0800

    day2 part2

--- bin/day2/day2.ml
+++ bin/day2/day2.ml
@@ -1,4 +1,6 @@
-let _test = {|7 6 4 2 1
+[@@@warning "-32"]
+
+let test = {|7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
@@ -27,29 +29,38 @@ module Solving = struct
       CCList.for_all (fun x -> 1 <= CCInt.abs x && CCInt.abs x <= 3) l
     in
     let diffs = first_difference l in
-    CCFormat.printf "@.Original: %a   Diffs: %a   Safe: %s"
-      CCFormat.Dump.(list int)
-      l
-      CCFormat.Dump.(list int)
-      diffs
-      (string_of_bool (same_sign diffs && within_bounds diffs));
     same_sign diffs && within_bounds diffs
 
   let num_is_safe (l : int list list) = CCList.filter is_safe l |> CCList.length
+
+  let is_safe_after_removal (l : int list) =
+    let len = CCList.length l in
+    let removed_lists =
+      CCList.map (fun i -> CCList.remove_at_idx i l) CCList.(0 -- (len - 1))
+    in
+    let bool_list = CCList.map is_safe removed_lists in
+    CCList.exists (fun x -> x = true) bool_list
+
+  let num_is_safe_after_removal (l : int list list) =
+    CCList.filter is_safe_after_removal l |> CCList.length
 end
 
 let () =
   let res =
     let open CCResult in
-    let+ reports = CCParse.parse_string Parsing.reports _test in
-    Solving.num_is_safe reports
+    let+ reports = CCParse.parse_string Parsing.reports test in
+    Solving.num_is_safe reports, Solving.num_is_safe_after_removal reports
   in
-  CCFormat.printf "@.(Part 1,Part 2) : %a@." CCFormat.Dump.(CCResult.pp int) res
+  CCFormat.printf "@.(Part 1,Part 2) : %a@."
+    CCFormat.Dump.(CCResult.pp (pair int int))
+    res
 
 let () =
   let res =
     let open CCResult in
     let+ reports = CCParse.parse_file Parsing.reports "bin/day2/input.txt" in
-    Solving.num_is_safe reports
+    Solving.num_is_safe reports, Solving.num_is_safe_after_removal reports
   in
-  CCFormat.printf "@.(Part 1,Part 2) : %a@." CCFormat.Dump.(CCResult.pp int) res
+  CCFormat.printf "@.(Part 1,Part 2) : %a@."
+    CCFormat.Dump.(CCResult.pp (pair int int))
+    res
