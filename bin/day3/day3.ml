3a0a1b42c54afb620e61ca85edfbb5be2487c4c1
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2024-12-03 11:21:29 -0800

    day3 part1 no regex

--- bin/day3/day3.ml
+++ bin/day3/day3.ml
@@ -6,22 +6,17 @@ let test =
 module Parsing = struct
   open CCParse
 
-  let get_muls input =
-    let regex = Str.regexp "mul(\\([0-9]+\\),\\([0-9]+\\))" in
-    let rec find_matches pos acc =
-      try
-        let start_pos = Str.search_forward regex input pos in
-        let match_str = Str.matched_string input in
-        find_matches (start_pos + 1) (match_str :: acc)
-      with Not_found -> List.rev acc
-    in
-    find_matches 0 []
-
   let instr =
     let* n1 = exact "mul(" *> U.int in
     let* n2 = char ',' *> U.int in
     let* _ = char ')' in
-    pure (n1, n2)
+    pure (Some (n1, n2))
+
+  let muls =
+    many
+    @@ (instr
+       <|> let+ _ = any_char in
+           None)
 end
 
 module Solving = struct
@@ -29,19 +24,18 @@ module Solving = struct
     CCList.fold_left ( + ) 0 (CCList.map (fun (x, y) -> x * y) l)
 end
 
-(* let () =
-   let mul_strings = Parsing.get_muls test in
-   let mul_ints =
-     CCList.map (CCParse.parse_string_exn Parsing.instr) mul_strings
-   in
-   CCFormat.printf "Part1 : %d@." (Solving.sum mul_ints) *)
-
 let () =
-  let test = CCIO.(read_all (open_in "bin/day3/input.txt")) in
-  let mul_strings = Parsing.get_muls test in
   let mul_ints =
-    CCList.map (CCParse.parse_string_exn Parsing.instr) mul_strings
+    CCParse.parse_string_exn Parsing.muls test
+    |> CCList.filter CCOption.is_some
+    |> CCList.map (CCOption.get_exn_or "failed")
   in
   CCFormat.printf "Part1 : %d@." (Solving.sum mul_ints)
 
-let () = ()
+let () =
+  let mul_ints =
+    CCParse.parse_file_exn Parsing.muls "bin/day3/input.txt"
+    |> CCList.filter CCOption.is_some
+    |> CCList.map (CCOption.get_exn_or "failed")
+  in
+  CCFormat.printf "Part1 : %d@." (Solving.sum mul_ints)
