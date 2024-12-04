323d756bb26c7f1537bde4b74eabb27e2db4049d
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2024-12-04 09:58:00 -0800

    day4 part1

--- bin/day4/day4.ml
+++ bin/day4/day4.ml
@@ -16,78 +16,45 @@ let spf = CCFormat.printf "@.%a" CCFormat.Dump.(array (array char))
 
 module Solving = struct
   module Infix = struct
-    let safe_get arr x y =
+    let safe_get arr (x, y) =
       if x >= 0 && y >= 0 && x < Array.length arr && y < Array.length arr.(x)
       then
         Some arr.(x).(y)
       else
         None
 
-    let ( <|> ) a b =
-      match a with
-      | Some _ -> a
-      | None -> b
+    let ( <?> ) = safe_get
   end
 
-  let check_forward arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r + 1).(c) = 'M'
-    && arr.(r + 2).(c) = 'A'
-    && arr.(r + 3).(c) = 'S'
+  let find_in_dirs arr ~init:(r, c) ~dxy:(dx, dy) =
+    let directions =
+      CCList.scan_left
+        (fun (x, y) (z, w) -> x + z, y + w)
+        (r, c)
+        (CCList.replicate 3 (dx, dy))
+    in
+    CCList.map (fun (x, y) -> Infix.(arr <?> (x, y))) directions
+    |> CCList.filter CCOption.is_some
 
-  let check_backward arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r - 1).(c) = 'M'
-    && arr.(r - 2).(c) = 'A'
-    && arr.(r - 3).(c) = 'S'
+  let search arr ((r, c) : int * int) =
+    let key = CCList.map CCOption.some [ 'X'; 'M'; 'A'; 'S' ] in
+    let dxys = [ 1, 0; -1, 0; 0, 1; 0, -1; 1, 1; -1, -1; 1, -1; -1, 1 ] in
+    let all_dirs =
+      CCList.map (fun dxy -> find_in_dirs arr ~init:(r, c) ~dxy) dxys
+    in
+    CCList.filter (fun l -> l = key) all_dirs |> CCList.length
 
-  let check_top arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r).(c + 1) = 'M'
-    && arr.(r).(c + 2) = 'A'
-    && arr.(r).(c + 3) = 'S'
-
-  let check_down arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r).(c - 1) = 'M'
-    && arr.(r).(c - 2) = 'A'
-    && arr.(r).(c - 3) = 'S'
-
-  let check_top_right arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r + 1).(c + 1) = 'M'
-    && arr.(r + 2).(c + 2) = 'A'
-    && arr.(r + 3).(c + 3) = 'S'
-
-  let check_bottom_left arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r - 1).(c - 1) = 'M'
-    && arr.(r - 2).(c - 2) = 'A'
-    && arr.(r - 3).(c - 3) = 'S'
-
-  let check_top_left arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r - 1).(c + 1) = 'M'
-    && arr.(r - 2).(c + 2) = 'A'
-    && arr.(r - 3).(c + 3) = 'S'
-
-  let check_bottom_right arr (r, c) =
-    arr.(r).(c) = 'X'
-    && arr.(r + 1).(c - 1) = 'M'
-    && arr.(r + 2).(c - 2) = 'A'
-    && arr.(r + 3).(c - 3) = 'S'
-
-  let search arr ((r, c) : int * int) = assert false
-
-  let search_array (mx : char array array) =
+  let search_array (arr : char array array) =
     let module A = CCArray in
-    let rows = A.length mx in
-    let columns = A.length mx.(0) in
+    let rows = A.length arr in
+    let columns = A.length arr.(0) in
+    let ans = ref 0 in
     for r = 0 to rows - 1 do
       for c = 0 to columns - 1 do
-        ()
+        ans := !ans + search arr (r, c)
       done
-    done
+    done;
+    !ans
 end
 
 module Parsing = struct
@@ -98,6 +65,28 @@ module Parsing = struct
   let of_filename s = CCIO.(read_all (open_in s)) |> of_string
 end
 
+(* let () =
+   let open Solving.Infix in
+   let open CCOption in
+   let mx = Parsing.of_filename "bin/day4/input.txt" in
+   let el = mx <?> (10, 0) <+> (mx <?> (11, 0)) <+> (mx <?> (12, 0)) in
+   CCFormat.printf "@.%a" CCFormat.Dump.(array char) mx.(0);
+   CCFormat.printf "@.%a" CCFormat.Dump.(CCFormat.some char) el *)
+
 let () =
-  let mx = Parsing.of_filename "bin/day4/input.txt" in
-  CCFormat.printf "@.%a" CCFormat.Dump.(array char) mx.(0)
+  let arr = Parsing.of_string test in
+  let ans = Solving.search_array arr in
+  CCFormat.printf "@.Part1: %d" ans
+
+let () =
+  let arr = Parsing.of_filename "bin/day4/input.txt" in
+  let ans = Solving.search_array arr in
+  CCFormat.printf "@.Part1: %d" ans
+
+(* let () =
+   (* let open Solving.Infix in *)
+   (* let open CCOption in *)
+   let mx = Parsing.of_filename "bin/day4/input.txt" in
+   let el = Solving.find_in_dirs mx ~init:(0, 0) ~dxy:(-1, -1) in
+   CCFormat.printf "@.%a" CCFormat.Dump.(array char) mx.(0);
+   CCFormat.printf "@.%a" CCFormat.Dump.(list (CCFormat.some char)) el *)
